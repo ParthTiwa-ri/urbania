22b24bdc7fcc2866045ef2c9004afb8eafaa8bb6
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-05-06 23:41:11 +0530

    heading center fix

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -12,9 +12,9 @@ export default function Head() {
         className="relative"
       ></Image>
       {/* <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-b from-transparent to-black opacity-50"></div> */}
-      <div className="absolute top-6 left-4 text-center">
+      <div className="absolute top-6 left-1/2  -translate-x-1/2">
         {/* <p className=" text-sm font-light">Good Morning ☀️</p> */}
-        <h1 className="ml-[15%] text-nowrap  font-bold text-3xl -mt-2 tracking-tight">
+        <h1 className=" text-nowrap  font-bold text-3xl -mt-2 tracking-tight">
           Book Your Trip Now
         </h1>
       </div>
