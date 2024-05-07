86bf95586189728d4029d163d98bb62f759a87ac
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-05-07 11:22:06 +0530

    section negative margin

--- src/components/DetailSection.tsx
+++ src/components/DetailSection.tsx
@@ -7,7 +7,7 @@ import PopularTour from "./PopularTour";
 import Category from "./Category";
 export default function DetailSection() {
   return (
-    <section className=" p-6 bg-zinc-200 w-full h-[100%] rounded-[30px] rounded-b-none -mt-4 z-10 relative">
+    <section className=" p-6 bg-zinc-200 w-full h-[100%] rounded-[30px] rounded-b-none -mt-6 z-10 relative">
       <div className="w-16 absolute top-3 left-1/2 -translate-x-1/2 h-2 rounded bg-zinc-400"></div>
       <PopularTour />
       <Category />
