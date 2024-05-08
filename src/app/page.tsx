a1165d449c5657e49c1b9964b5489a3b6cd5399f
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-05-08 17:42:12 +0530

    base-url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,16 @@
-import React from "react";
+import DetailSection from "@/components/DetailSection";
+import Head from "@/components/Head";
+import axios from "axios";
 
-export default function page() {
-  return <div></div>;
+export default async function Home() {
+  const res = await axios.get(`${process.env.BASE_URL}/api/tour/`);
+  const data = res.data;
+
+  console.log(data[0]);
+  return (
+    <main className="bg-black max-h-screen">
+      <Head />
+      <DetailSection />
+    </main>
+  );
 }
-
-// import DetailSection from "@/components/DetailSection";
-// import Head from "@/components/Head";
-// import axios from "axios";
-
-// export default async function Home() {
-//   const res = await axios.get(`${process.env.BASE_URL}/api/tour/`);
-//   const data = res.data;
-
-//   console.log(data[0]);
-//   return (
-//     <main className="bg-black max-h-screen">
-//       <Head />
-//       <DetailSection />
-//     </main>
-//   );
-// }
