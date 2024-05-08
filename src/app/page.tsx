627995b7ae1a97feecf0ee2b6241757aa6ec0526
Parth Tiwari <parthtiwari@Parths-MacBook-Air.local>
2024-05-08 18:24:07 +0530

    api fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import DetailSection from "@/components/DetailSection";
-import Head from "@/components/Head";
-import axios from "axios";
+// import DetailSection from "@/components/DetailSection";
+// import Head from "@/components/Head";
+// import axios from "axios";
 
-export default async function Home() {
-  return (
-    <main className="bg-black max-h-screen">
-      <Head />
-      <DetailSection />
-    </main>
-  );
+// export default async function Home() {
+//   const res = await axios.get(`api/tour/`);
+//   const data = res.data;
+
+//   console.log(data[0]);
+//   return (
+//     <main className="bg-black max-h-screen">
+//       <Head />
+//       <DetailSection />
+//     </main>
+//   );
+// }
+import React from "react";
+
+export default function page() {
+  return <div></div>;
 }
